752ae2a0e9cd9fb16f3ef31e844751b2b90178d3
2388853 <2388853@AFRICA.AIRTEL.ITM>
2020-04-26 20:11:16 +0530

    Updates

---
+++ precache-manifest.48d76a36f59007ec8deeb1b9a010b24c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b7028b33d9e67a5b97a57133b85ef7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6e321ffc2b89f392e2f1",
+    "url": "/monsters-rolodex/static/css/main.a6927197.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e321ffc2b89f392e2f1",
+    "url": "/monsters-rolodex/static/js/main.c47a2932.chunk.js"
+  },
+  {
+    "revision": "0c81cb278443448182f0",
+    "url": "/monsters-rolodex/static/js/runtime-main.165419b0.js"
+  }
+]);
